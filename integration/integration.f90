a2dbc16e71874a75f41893bac07293a3e8d71d25
morten-engsvang <engsvang.morten@gmail.com>
2023-05-09 15:37:18 +0200

    integration half done

--- integration/integration.f90
+++ integration/integration.f90
@@ -3,5 +3,11 @@ PROGRAM integration
         IMPLICIT NONE
         INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(15,307)
         REAL(dp), PARAMETER :: pi = 3.1415926535897932
-        CALL integrate
+        REAL(dp), PARAMETER :: a = 0 ! Start of range
+        REAL(dp), PARAMETER :: b = pi/2.0 ! End of range
+        INTEGER :: m ! # of increments
+        REAL(dp) :: value ! Value of the integral
+        DO m = 1, 20, 1
+                CALL value = integrate(a,b,m)
+        END DO
 END PROGRAM integration
--- integration/integration_mod.f90
+++ integration/integration_mod.f90
@@ -2,10 +2,17 @@ MODULE integration_mod
     IMPLICIT NONE
     INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(15,307)
 CONTAINS
-    FUNCTION integrate(arg) RESULT(return)
-        !integer, intent(in) :: arg
+    FUNCTION integrate(a,b,m) RESULT(return)
+        REAL(dp), PARAMETER :: a, b ! Start and end of range
+        REAL(dp) :: c 
+        REAL(dp) :: pos ! Current position
+        INTEGER :: m ! # of increments
+        INTEGER :: i 
+        REAL(dp) :: h = (b-a)/m !Increment
         REAL(dp) :: return
-    
-        
+        pos = a
+        DO i = 0, m, 1
+            
+        END DO
     END FUNCTION integrate
 END MODULE integration_mod
