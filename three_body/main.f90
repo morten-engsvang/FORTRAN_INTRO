b5fe272c9263acabb32cc4e1e40f3ee18b23e07c
morten-engsvang <engsvang.morten@gmail.com>
2023-06-06 10:12:49 +0300

    change to lowercase

--- three_body/main.f90
+++ three_body/main.f90
@@ -1,14 +1,14 @@
-PROGRAM three_body
-        USE rkf45_mod, ONLY : ! Figure out which function I need
-        IMPLICIT NONE
-        INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(15,307)
-        REAL(dp), PARAMETER :: pi = 3.1415926535897932
-        REAL(dp) :: t = 0 ! Current time, starts at t = 0
-        REAL(dp) :: t_end = 1000 ! End time, maybe it should be written in input.dat?
+program three_body
+        use rkf45_mod, only : ! Figure out which function I need
+        implicit none
+        integer, parameter :: dp = SELECTED_REAL_KIND(15,307)
+        real(dp), parameter :: pi = 3.1415926535897932
+        real(dp) :: t = 0 ! Current time, starts at t = 0
+        real(dp) :: t_end = 1000 ! End time, maybe it should be written in input.dat?
 
-        DO m = 1, 10000, 1
-                WRITE(*,*) "value of m: ", m
+        do m = 1, 10000, 1
+                write(*,*) "value of m: ", m
                 value = integrate(a,b,m)
-                WRITE(*,*) "integral value of: ", value
-        END DO
-END PROGRAM three_body
+                write(*,*) "integral value of: ", value
+        end do
+end program three_body
