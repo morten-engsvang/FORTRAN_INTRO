0f90ee1145ca36caf7a752ba42017b2edb8c3822
morten-engsvang <engsvang.morten@gmail.com>
2023-06-06 11:24:19 +0300

    changed to actually make use of Runge-Kutta subroutine

--- three_body/main.f90
+++ three_body/main.f90
@@ -1,9 +1,8 @@
 program three_body
-        use rkf45_mod, only : ! Figure out which function I need
+        use rkf45_mod, only : r8_rkf45 ! Figure out which function I need
         implicit none
         integer, parameter :: dp = selected_real_kind(15,307) ! double precision
         real(dp), parameter :: pi = 3.1415926535897932
-        real(dp), parameter :: time_increment = 1.0 ! Size of the time-step
         real(dp) :: time_start = 0 ! Start time, starts at t = 0
         real(dp) :: time_end = 1000 ! End time, maybe it should be written in input.dat?
         real(dp) :: time ! Current time
@@ -20,28 +19,11 @@ program three_body
         ! Code :)
         ! Calculate acceleration at the starting position
         ! Code :)
+        time = time_start
+        ! Initialization done, start the integration:
+        
+        do while (time <= time_end)
+                call r8_rkf45 ( f, neqn, y, yp, t, tout, relerr, abserr, flag )
+        end do
 
-        ! Initialization done, start the time-loop:
-        do time=time_start, time_end, time_increment
-                ! Update positions, using velocity
-                x1 = x1 + vel_x1*time_increment
-                x2 = x2 + vel_x2*time_increment
-                x3 = x3 + vel_x3*time_increment
-                y1 = y1 + vel_y1*time_increment
-                y2 = y2 + vel_y2*time_increment
-                y3 = y3 + vel_x3*time_increment
-                ! Update velocity for the new position
-                vel_x1 = vel_x1 + acc_x1
-                vel_x2 = vel_x2 + acc_x2
-                vel_x3 = vel_x3 + acc_x3
-                vel_y1 = vel_y1 + acc_y1
-                vel_y2 = vel_y2 + acc_y2
-                vel_y3 = vel_y3 + acc_y3
-                ! Calculate acceleration at the new position
-        end do
-        do m = 1, 10000, 1
-                write(*,*) "value of m: ", m
-                value = integrate(a,b,m)
-                write(*,*) "integral value of: ", value
-        end do
 end program three_body
